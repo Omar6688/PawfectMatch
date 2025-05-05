510afe06f934c5e0bbb0d7d448337f2415288abb
Omar Bitar <omarbitar88@gmail.com>
2025-05-05 13:10:55 +0200

    Create base.html and Add Pet model with name, breed, age, description, and image_url

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    breed = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    description = models.TextField()
+    image_url = models.URLField(blank=True)  # For simplicity, using URL-based images
+
+    def __str__(self):
+        return self.name
