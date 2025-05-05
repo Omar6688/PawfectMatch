6b927df484d01b7b46294734e2f026c979b11c2c
Omar Bitar <omarbitar88@gmail.com>
2025-05-05 13:59:19 +0200

    Add Pet detail view with dynamic URL and template

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('pet/<int:pk>/', views.pet_detail, name='pet_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Pet
 
 def home(request):
     pets = Pet.objects.all()
     return render(request, 'core/home.html', {'pets': pets})
+
+def pet_detail(request, pk):
+    pet = get_object_or_404(Pet, pk=pk)
+    return render(request, 'core/pet_detail.html', {'pet': pet})
