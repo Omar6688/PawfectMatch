a7b02fee97f47d06200312258dfc6966a290c4e7
Omar Bitar <omarbitar88@gmail.com>
2025-05-05 13:37:56 +0200

    Display Pet model data on homepage using Bootstrap cards

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Pet
 
 def home(request):
-    return render(request, 'core/home.html')
+    pets = Pet.objects.all()
+    return render(request, 'core/home.html', {'pets': pets})
