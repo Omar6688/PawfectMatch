956569bbeb568a055e3419613c2620c4b38549f1
Omar Bitar <omarbitar88@gmail.com>
2025-05-06 14:48:59 +0200

    Install and configure django-crispy-forms with Bootstrap 5

--- pawfectmatch_project/settings.py
+++ pawfectmatch_project/settings.py
@@ -48,7 +48,14 @@ INSTALLED_APPS = [
     'bookings',
     'support',
     'adoptions',
+
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
