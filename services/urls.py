e0d18ed2801325d04eab9b4283533f9ca967d935
Omar Bitar <omarbitar88@gmail.com>
2025-05-08 00:34:43 +0200

    Add booking success page and connect Stripe success_url

--- services/urls.py
+++ services/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.service_list, name='service_list'),
+    path('book/<int:service_id>/', views.create_service_checkout, name='make_booking'),
+    path('booking/success/', views.booking_success, name='booking_success'),
+
 ]
