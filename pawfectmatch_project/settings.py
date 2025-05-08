8ffaa1052fa4e6631eab6dfdff2085b609acd385
Omar Bitar <omarbitar88@gmail.com>
2025-05-09 00:32:21 +0200

    Fix: Added Heroku URL to ALLOWED_HOSTS

--- pawfectmatch_project/settings.py
+++ pawfectmatch_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-for-dev-only')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['pawfectmatch-ok.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['pawfectmatch-ok-c783577133e6.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
