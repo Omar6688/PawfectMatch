3ca2163ec68adb591856eb7adf1cc7eab0d90deb
Omar Bitar <omarbitar88@gmail.com>
2025-05-10 17:22:01 +0200

    Finalize AWS S3 media storage setup

--- pawfectmatch_project/settings.py
+++ pawfectmatch_project/settings.py
@@ -178,9 +178,23 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-# Media files (uploaded content like images)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
+# AWS S3 MEDIA STORAGE SETUP
+if os.getenv('USE_AWS') == 'True':
+    AWS_STORAGE_BUCKET_NAME = 'pawfectmatch-media'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+
+    # MEDIA file storage only
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+
+# Fallback for local development
+else:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Stripe
