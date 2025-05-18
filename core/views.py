9edf8dc4b59aab163096133a8e1729219b44a4d6
Omar Bitar <omarbitar88@gmail.com>
2025-05-18 04:08:15 +0200

    Add user profile page and navbar link

--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from adoptions.models import AdoptablePet
 
 def home(request):
     pets = AdoptablePet.objects.all()
     return render(request, 'core/home.html', {'pets': pets})
+
+@login_required
+def profile_view(request):
+    """View for displaying user profile."""
+    return render(request, 'core/profile.html')
