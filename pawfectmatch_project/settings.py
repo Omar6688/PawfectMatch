d8ee63775d9ddabafe79c01aceac8a8a40bc9121
Omar Bitar <omarbitar88@gmail.com>
2025-05-20 09:50:39 +0200

    Cleaned up print statements before submission

--- pawfectmatch_project/settings.py
+++ pawfectmatch_project/settings.py
@@ -16,9 +16,6 @@ import dj_database_url
 
 from dotenv import load_dotenv
 load_dotenv()
-print("RAW USE_AWS:", os.getenv('USE_AWS'))
-print("RAW DEVELOPMENT:", os.getenv('DEVELOPMENT'))  # Confirm it's True
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
